26ef9ef5544f085522677abd49cbe0741c01c0b6
Sayeed <sayeed.afridi2009@gmail.com>
2016-02-15 21:23:56 +0530

    Linaro-GCC-5.2.1

---
+++ lib/gcc/x86_64-unknown-linux-gnu/5.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-linaro/configure --target= --prefix=/home/sayeed99/lin/sayeed/ --with-newlib --with-gnu-as --with-gnu-ld --with-fpmath=sse --enable-languages=c,c++ --disable-multilib --enable-shared --disable-libssp --with-gcc-version=Flare-Linaro";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
